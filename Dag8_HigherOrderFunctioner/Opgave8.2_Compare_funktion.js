043ab4a66689c11ee3abb0fb8c7a3c6a588b3261
Oleand <magnus.s.larsen@hotmail.com>
2022-09-26 20:57:25 +0200

    Kommet til opgave 8.2

---
+++ Dag8_HigherOrderFunctioner/Opgave8.2_Compare_funktion.js
@@ -0,0 +1,18 @@
+//Opretter personer. 
+let person1 = {name:"Bent Hansen", alder: "45", mobilNummer: "22334455"};
+let person2 = {name:"Jens Olsen", alder: "22", mobilNummer: "88888888"};
+let person3 = {name:"Mården karlsen", alder: "32", mobilNummer: "67847232"};
+let person4 = {name:"Bentebent", alder: "37", mobilNummer: "80808983"};
+let person5 = {name:"Magnus Larsen", alder: "27", mobilNummer: "28715538"};
+
+let personer = []; 
+
+personer.push(person1);
+personer.push(person2);
+personer.push(person3);
+personer.push(person4);
+personer.push(person5);
+
+personer.forEach(p => console.log(p));
+
+
