adf61cc4d24910eb3a5382cc3ee03f83d6a00843
Oleand <magnus.s.larsen@hotmail.com>
2022-10-22 00:48:28 +0200

    alt

--- Dag12/JSON og HTTP/opgave12.2.js
+++ Dag12/JSON og HTTP/opgave12.2.js
@@ -1,7 +1,9 @@
-const alleChatrum = "https://beskedserver.azurewebsites.net/api/chatRum/"
+const alleChatrum = "https://beskedserver.azurewebsites.net/api/chatRum/";
+const soegbeskedIRum = 'https://beskedserver.azurewebsites.net/api/SoegBeskeder/';
+const beskedPost = 'https://beskedserver.azurewebsites.net/api/Beskeder/';
 
 visrum(alleChatrum);
-document.querySelector("#0").onclick = visBeskeder;
+let aktuelRum;
 
 async function visrum(url) {
     try {
@@ -9,22 +11,57 @@ async function visrum(url) {
         console.log(respons)
         let tabel = document.querySelector("#tabel");
         for(let i = 0; i< respons.length; i++){
-              tabel.innerHTML = tabel.innerHTML + "<tr id=" + i +"> " + respons[i].navn + "</tr>"
+            tabel.innerHTML = tabel.innerHTML + "<tr> <input type=button onclick=visBeskeder('" + respons[i].navn + "') value =" + respons[i].navn +" ></input> </tr>"
         }
     } catch (fejl) {
         console.log(fejl);
  }
 }
 
-async function visBeskeder(){
+ async function visBeskeder(rum){
     try {
-        let respons = await get("https://beskedserver.azurewebsites.net/api/SoegBeskeder/{rum}"); //referere til filen, get.js
-        //console.log(respons)
-        console.log("Test")
-        //let tabel = document.querySelector("#tabel");
-        //
+        aktuelRum = rum;
+        let respons = await get(soegbeskedIRum + rum); //referere til filen, get.js
+        let beskeder = document.querySelector("#beskeder");
+        console.log(respons);
+        let nyInnerHTML = "";
+        for(let b of respons){
+            nyInnerHTML = nyInnerHTML + "<br><label>"+ b.id +" "+ b.tekst +" "+ b.chatRum+ "</label><br>"
+        }
+        beskeder.innerHTML = nyInnerHTML; //Viser kun de beskeder jeg ønsker fra den bestemte chat. 
+
     } catch (fejl) {
         console.log(fejl);
  }
+ let ButtonOgTekstField = document.querySelector("#buttonOgTekstField");
+ ButtonOgTekstField.innerHTML = " <br/> <label>Tekst</label> <input type=text id=tekstField value =" + "" + "> <input type=button value=Opret onclick=postBesked()>"
+    
+ }
+
+
+async function postBesked() {
+    let tekst = document.querySelector("#tekstField").value;
+    let besked = { Tekst: tekst, ChatRum: aktuelRum};
+    try {
+        let respons = await post(beskedPost,besked);
+        console.log(respons);
+    } catch (fejl) {
+        console.log(fejl);
+    }
+    visBeskeder(aktuelRum);
 }
+
+async function post(url, objekt) {
+    const respons = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(objekt),
+        headers: { 'Content-Type': 'application/json' }
+    });
+    if (respons.status !== 204) // Created
+        throw new Error(respons.status);
+    return await respons.text();
+}
+
+
+
 
