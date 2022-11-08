f015700ba699f88559c2f48fe63c754e45e72c73
Oleand <magnus.s.larsen@hotmail.com>
2022-11-08 22:19:55 +0100

    dd

--- Server_Arbejder/server.js
+++ Server_Arbejder/server.js
@@ -1,24 +1,26 @@
 const express = require("express")
 const app = express();
+
 let path = require('path'); //Kun vigtige fordi den ligger i mappe. 
 
-app.set("view engine","ejs")
+app.set("view engine","pug")
 app.set('views', path.join(__dirname, '/views')); // kun vigtig da den ligger i mappe. 
 
-let beskeder = [{ afsender: "Ole", tekst: "Min første besked", chatrum: "rum1", nummer: "1" }, { afsender: "Ib", tekst: "Hallo, er der nogen?", chatrum: "rum2", nummer: "2" }];
-let chatRum = [{ navn: "rum1" }, { navn: "rum2" }];
 
-let valueForView = {Chatrums: chatRum};
+let beskeder = [{ afsender: "Ole", tekst: "Min første besked", chatrum: "rum1", nummer: "1" }, { afsender: "Ib", tekst: "Hallo, er der nogen?", chatrum: "rum2", nummer: "2" }];
+let chatRum = [{ navn: "Rum1" }, { navn: "rum2" }];
+
+let valueForView = {chatRooms: chatRum, messeges: beskeder};
 
 app.get("/",(reg,res) =>{
     console.log("Here")
     //res.send("hello")
-    res.render('index',valueForView)
+    res.render('index2',valueForView)
 })
 
-const userRouter = require('./routes/users')
+const userRouter = require('./routes/router')
 
-app.use('/users', userRouter)
+app.use('/', userRouter)
 
 app.listen(9000);
 
